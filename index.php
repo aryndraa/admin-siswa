b8dc1cb642e1bb5716742b44f56e11c9d8f4f0ba
griyadrs <maderestu775@gmail.com>
2024-07-22 07:44:21 +0700

    file 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    require "session.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h2>Halo Admin</h2>
+    <div>
+        <button name="logoutbtn" class="btn btn-success form-control mt-3" type="submit">
+        Login</button>
+    </div>
+</body>
+</html>
